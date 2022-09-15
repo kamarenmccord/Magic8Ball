75f3b7847bbb97e7f739c3445262a9c6354dd9e7
Tobi <tobi@pop-os.localdomain>
2022-09-15 09:43:51 -0500

    updated joke

--- logic/settings.py
+++ logic/settings.py
@@ -5,6 +5,6 @@ HEIGHT = 720
 FPS = 60
 CUSTOM_TITLE = "Magic 8 Ball"
 
-# this is where I would put my color schema, if i had any
+# this is where I would put my color schema, if i had one
 COLOR_BACKGROUND = "#80bfff"
 BAR_COLOR = "#1a8cff"
